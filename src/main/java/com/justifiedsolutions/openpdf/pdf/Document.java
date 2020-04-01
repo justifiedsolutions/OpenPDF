66244b7afcd5606a558500333880370a9943b0a9
Jay Burgess <jragingfury@gmail.com>
2020-04-01 15:22:05 -0600

    Add copyright to files

--- src/main/java/com/justifiedsolutions/openpdf/pdf/Document.java
+++ src/main/java/com/justifiedsolutions/openpdf/pdf/Document.java
@@ -1,3 +1,9 @@
+/*
+ * SPDX-License-Identifier: (LGPL-3.0-only OR MPL-2.0)
+ *
+ * Copyright (c) 2020 Justified Solutions. All rights reserved.
+ */
+
 package com.justifiedsolutions.openpdf.pdf;
 
 import com.justifiedsolutions.openpdf.pdf.content.Chapter;
--- src/main/java/com/justifiedsolutions/openpdf/pdf/font/PDFNativeFont.java
+++ src/main/java/com/justifiedsolutions/openpdf/pdf/font/PDFNativeFont.java
@@ -1,3 +1,9 @@
+/*
+ * SPDX-License-Identifier: (LGPL-3.0-only OR MPL-2.0)
+ *
+ * Copyright (c) 2020 Justified Solutions. All rights reserved.
+ */
+
 package com.justifiedsolutions.openpdf.pdf.font;
 
 import java.awt.Color;
